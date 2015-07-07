8a1178e44c5813731076609de8dfd5dfc01fc6a1
Arthur de Araújo Farias <arthur@afarias.org>
2015-07-07 12:16:43 -0300

    Modified to original template.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,9 @@
 #include "lib/main.h"
 
 void setup(void) {
-	pinMode(13, output);
 	return ;
 }
 
 void loop(void) {
-	delay(500);
-	digitalWrite(13, HIGH);
-	delay(500);
-	digitalWrite(13, LOW);
 	return ;
 }
