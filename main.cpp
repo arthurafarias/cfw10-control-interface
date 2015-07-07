ae560c12c729e3f10786e0316aba000b63f8acf8
Arthur de Araújo Farias <arthur@afarias.org>
2015-07-07 12:12:42 -0300

    BLink logic added

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 #include "lib/main.h"
 
 void setup(void) {
+	pinMode(13, output);
 	return ;
 }
 
 void loop(void) {
+	delay(500);
+	digitalWrite(13, HIGH);
+	delay(500);
+	digitalWrite(13, LOW);
 	return ;
 }
